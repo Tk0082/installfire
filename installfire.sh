f7f778cd8e91314358eb800cd910f2e2889cbbc9
tk0082 <tk0082@hotmail.com>
2024-04-17 17:40:08 -0300

    Download automátioco do arquivo na instalação

--- installfire.sh
+++ installfire.sh
@@ -14,10 +14,11 @@
 #	1.3 - 15.04.2020 - Incremento da configuração do programa no sistema	#
 #	1.4 - 22.05.2020 - Dando uma avivada no programa, com cores		#
 #	1.5 - 17.10.2021 - Correção de Opções para Usuário Root			#
+#	1.6 - 17.04.2024 - Download automático dos arquivos na instalação	#
 #										#
 #################################################################################
 
-VS='Versão-1.5'
+VS='Versão-1.6'
 
 # CORES ==========================
 R="[38;5;160m"  # Red
@@ -32,21 +33,21 @@ Z="[0m"  # Zerar
 
 l="---------------------------------------------------------------------------------------"
 prog=$(echo $0 | sed 's/.*\///g')
-usrX=$USER;
 dir="/usr/share/firefox"
-arq1=$(pwd/$2);
+arq1=$(pwd $2)
+
 
 #if [ "$usrX" != "root" ]; 
 #then
 #	arq="$HOME/Downloads/firefox*"
 #	desk="/home/$usrX/Desktop/firefox.desktop"
 	
-#	if [ ! -f "$arq1" ];
-#	then
-#		v=$(ls /home/$usrX/Downloads/firefox* | sed 's/.*-//g;s/.tar.*//g')
-#	else
-##		v=$(ls $arq1 | sed 's/.*-//g;s/.tar.*//g')
-#	fi
+	if [ ! -f "$arq1" ];
+	then
+		v=$(ls $HOME/Downloads/firefox* | sed 's/.*-//g;s/.tar.*//g')
+	else
+		v=$(ls $arq1 | sed 's/.*-//g;s/.tar.*//g')
+	fi
 #else
 	arq="$HOME/Downloads/firefox*"
 	desk="$HOME/Desktop/firefox.desktop"
@@ -60,7 +61,7 @@ arq1=$(pwd/$2);
 #fi
 
 # https://www.mozilla.org/pt-BR/firefox/download/thanks/	( Link direto arqv br.fire.bz2 ) 
-#https://download-installer.cdn.mozilla.net/pub/firefox/releases/125.0.1/linux-x86_64/pt-BR/firefox-*.tar.bz2
+#https://download-installer.cdn.mozilla.net/pub/firefox/releases/125.0.1/linux-x86_64/pt-BR/firefox-125.0.1.tar.bz2
 
 msg="
 $C$N$l
@@ -71,7 +72,7 @@ $C$N$l
   $G$F      installfire [$C-c, -i, -h, -r, -v$G] firefox.zip $Z
 
   $C$N	-c -- Configurar o$G$F $prog $Z$C$N no Sistema
-	-i -- Instalar o Firefox
+	-i -- Instalar o Firefox, caso não tenha feito o download do zip o download será feito automático
 	-h -- Help, informações de uso
 	-r -- Remover o Firefox
 	-v -- Versão
@@ -98,7 +99,7 @@ config(){
 	local=$(pwd)
 	if [ ! -d "/usr/share/installfire" ]; then
 		sudo mkdir /usr/share/installfire/
-		sudo cp $local/installfire /usr/share/installfire/
+		sudo cp $local/installfire.sh /usr/share/installfire/
 		sudo chown root:root /usr/share/installfire/*
 		sudo chmod 755 /usr/share/installfire/*
 		sudo ln -s /usr/share/installfire/installfire /usr/bin/instfire
@@ -108,7 +109,7 @@ config(){
   	else
 		echo -e "$F$G\n\t\tDiretório encontrado, configurndo Programa..\n$Z"
 
-		sudo cp $local/installfire /usr/share/installfire/
+		sudo cp $local/installfire.sh /usr/share/installfire/
 		sudo chown root:root /usr/share/installfire/*
 		sudo chmod 755 /usr/share/installfire/*
 		sudo ln -s -f /usr/share/installfire/installfire /usr/bin/instfire
@@ -118,40 +119,6 @@ config(){
 	fi
 }
 
-	# Download e instalação direta do Firefox PT-BR (Configurar o link para sua lingua )================
-download(){
-	clear
-	pkill firefox
-	rm -f $HOME/Downloads/firefox*
-	if [ ! -f $arq ]; 
-	then
-		clear
-		echo -e "$F$R\n\t\tArquivo não encontrado em Downloads!\n\n"
-		sleep .5
-		echo -e "$G$F==[ Baixando o$C$F Firefox$G$F ]================================"
-		wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/125.0.1/linux-x86_64/pt-BR/firefox-125.0.1.tar.bz2 -P $HOME/Downloads/
-		sleep .5
-		clear
-		if [ "$usrX" != "root" ];
-		then
-			sudo tar -vxjf $arq -C $dir --overwrite 
-			exit 0
-		else
-			tar -vxjf $arq -C $dir --overwrite 
-			exit 0
-		fi
-	fi
-	sleep 1
-	clear
-
-	echo -e "$G$F\t\tCriando atalho..$Z"
-	touch $desk
-	ico
-	chmod +x $desk
-	chown $USER:$USER $desk
-	sudo cp $desk /usr/share/applications/
-
-}
 
 	# Verificar se o diretório existe, caso contrário, cria ====================
 pathfire(){
@@ -169,14 +136,14 @@ remove(){
 	clear
 	echo -e "$R$F\t\tRemovendo o firefox..$Z"
 
-	if [ "$usrX" != "root" ]; then
+	if [ "$USER" != "root" ]; then
 		sudo rm -rf /usr/share/firefox/*
 		sudo rm -f /usr/share/applications/firefox*
-		sudo rm -f /home/$usrX/Desktop/firefox.*
+		sudo rm -f $HOME/Desktop/firefox.*
 	else
 		rm -rf /usr/share/firefox/*
 		rm -f /usr/share/applications/firefox*
-		rm -f /$usrX/Desktop/firefox.*
+		rm -f $HOME/Desktop/firefox.*
 	fi
 
 	clear
@@ -187,24 +154,43 @@ remove(){
 	clear
 }
 
+	# Download e instalação direta do Firefox PT-BR (Configurar o link para sua lingua )================
+download(){
+	clear
+	if [ ! -f $arq ]; 
+	then
+		clear
+		echo -e "$F$R\n\t\tArquivo não encontrado em Downloads!\n\n"
+		sleep .5
+		
+		echo -e "$G$F==[ Baixando o$C$F Firefox$G$F ]================================"
+
+		#== MUDE AQUI A VERSÃO DO DOWNLOAD [ /releases/**  e  nome do arquivo-**.tar ]==============
+		wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/125.0.1/linux-x86_64/pt-BR/firefox-125.0.1.tar.bz2 -P $HOME/Downloads/
+		sleep .5
+	fi
+}
+
 	# Extração do arqv .tar ====================
 install(){
 	clear
 	pkill firefox
 	sleep 1
-	echo -e "$G$F\n\t\tDescompactando arquivos.. \n$C$N"
 
-	if [ "$usrX" != "root" ];
+	if [ "$USER" != "root" ];
 	then
 		if [ ! -f $arq ]; 
 		then		
 			clear
 			echo -e "$F$R\n\t\tArquivo não encontrado em Downloads!
-		Faça o download do arquivo em$F$G https://www.mozilla.org/pt-BR/firefox/download/thanks/\n\n"
-			echo -ne "$F$R Pressione 'Enter' para sair: $Z"; read
+		$F${C}Vamos tentar baixá-lo em$F$G https://download-installer.cdn.mozilla.net/pub/firefox/releases/125.0.1/linux-x86_64/
+		$F${C}Verifique seu idioma no link de download, este download tem por padrão $F${G}pt-BR.\n\n"
+			echo -ne "$F$C Pressione 'Enter' para sair: $Z"; read
 			clear
-			exit 1
+			download 
+			install
 		else
+			echo -e "$G$F\n\t\tDescompactando arquivos.. \n$C$N"
 			sudo tar -vxjf $arq -C $dir --overwrite 
 		fi
 	else
@@ -212,11 +198,14 @@ install(){
 		then
 			clear
 			echo -e "$F$R\n\t\tArquivo não encontrado em Downloads!
-		Faça o download do arquivo em$F$G https://www.mozilla.org/pt-BR/firefox/download/thanks/\n\n"
-			echo -ne "$F$R Pressione 'Enter' para sair: $Z"; read
+		$F${C}Vamos tentar baixá-lo em$F$G https://download-installer.cdn.mozilla.net/pub/firefox/releases/125.0.1/linux-x86_64/
+		$F${C}Verifique seu idioma no link de download, este download tem por padrão $F${G}pt-BR.\n\n"
+			echo -ne "$F$C Pressione 'Enter' para sair: $Z"; read
 			clear
-			exit 1
+			download 
+			install
 		else
+			echo -e "$G$F\n\t\tDescompactando arquivos.. \n$C$N"
 			tar -vxjf $arq -C $dir --overwrite 
 		fi
 	fi
@@ -253,20 +242,6 @@ case $1 in
 		exit 0
 	done
 	;;
-   -d )
-	pathfire
-	download
-	while [ download != True ];
-	do
-		clear
-		echo $G$F$l
-		echo -e "\t\tFirefox instalado com sucesso!!"
-		echo $l$Z
-		sleep 1.5
-		clear
-		exit 0
-	done
-	;;
    -i ) 
 	pathfire
 	install
@@ -296,5 +271,7 @@ case $1 in
 	;;
 esac
 
-# Tk082_
+
+#== I.G.W.T ====================
+# Tk082
 
